c6ece443916c0a71b668f0bf18cca51e12f219c9
Miguel T Rivera <mrivera108@outlook.com>
2022-01-26 23:48:47 -0800

    Block in content for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import landscape from './image.svg'
 import './styles/main.css'
 
 function App() {
   return (
       <>
         <section className="container shadow">
+            <p>Upload your image</p>
+            <p>File should be image (jpeg, png)..</p>
+            <div className="box">
+                <img src={landscape} />
+                <p>Drag &amp; Drop your image here</p>
+            </div>
+            <p>Or</p>
+            <button>Choose a File</button>
         </section>
       </>
 
